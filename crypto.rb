3cf07ce4fa6b7ce8b7dbcc20e2fbe54c98f88f84
Luciano Scarpaci <16402942+lucianoscarpaci@users.noreply.github.com>
2024-01-29 10:31:33 -0500

    Update crypto library

--- crypto.rb
+++ crypto.rb
@@ -2,8 +2,8 @@ require 'openssl'
 require 'base64'
 
 # define the key and the IV for AES-256 bit
-key = OpenSSL::Cipher.new('AES-256-CBC').random_key
-iv = OpenSSL::Cipher.new('AES-256-CBC').random_iv
+aes_key = OpenSSL::Cipher.new('AES-256-CBC').random_key
+aes_iv = OpenSSL::Cipher.new('AES-256-CBC').random_iv
 # print the key from AES 256
 # puts "Key: #{key.unpack('H*').first}"
 # generate a new RSA key pair consisting of a public key and a private key
@@ -19,16 +19,27 @@ iv = OpenSSL::Cipher.new('AES-256-CBC').random_iv
 # puts "Private Key: #{rsa_key_pair[1].to_pem}"
 # the public key will be used for encryption, while the private key
 # will be used for decryption.
-def encrypt(plaintext, key, iv)
-	cipher = OpenSSL::Cipher.new('AES-256-CBC')
-	cipher.encrypt
-	cipher.key = key
-	cipher.iv = iv
-	encrypted = cipher.update(plaintext) + cipher.final
-	return encrypted
+rsa_key = OpenSSL::PKey::RSA.generate(2048)
+
+
+def encrypt(plaintext, aes_key, aes_iv, rsa_key)
+	aes_cipher = OpenSSL::Cipher.new('AES-256-CBC')
+	aes_cipher.encrypt
+	aes_cipher.key = aes_key
+	aes_cipher.iv = aes_iv
+	encrypted_data = aes_cipher.update(plaintext) + aes_cipher.final
+	rsa_public_key = rsa_key.public_key
+	encrypted_key = rsa_public_key.public_encrypt(aes_key)
+	encoded_data = Base64.strict_encode64(encrypted_data)
+	block_length = 31
+	header = "------ BEGIN RSA MESSAGE ------"
+	block_encoded_data = encoded_data.scan(/.{1,#{block_length}}/).join("\n")
+	footer = "------ END RSA MESSAGE ------"
+	message_block = "#{header}\n\n#{block_encoded_data}\n\n#{footer}"
+	return message_block, encrypted_key
 end
 
-def decrypt(ciphertext, key, iv)
+def decrypt(ciphertext, key, iv, rsa_key)
 	cipher = OpenSSL::Cipher.new('AES-256-CBC')
 	cipher.decrypt
 	cipher.key = key
@@ -41,32 +52,33 @@ print 'Enter the message to be encrypted: '
 plaintext = gets.chomp
 
 # Encryption
-ciphertext = encrypt(plaintext, key, iv)
-puts "Ciphertext: #{ciphertext.unpack('H*').first}"
+message_block, encrypted_key = encrypt(plaintext, aes_key, aes_iv, rsa_key)
+#puts "Ciphertext: #{ciphertext.unpack('H*').first}"
+puts "#{message_block}"
+puts "#{encrypted_key}"
 
 # Load the RSA public key
-rsa_key = OpenSSL::PKey::RSA.generate(2048)
-public_key = rsa_key.public_key
+
 
 # encrypt the ciphertext using RSA public key
-encrypted_cipher = public_key.public_encrypt(ciphertext)
-header = "------ BEGIN RSA MESSAGE ------"
-footer = "------ END RSA MESSAGE ------"
-encoded_cipher = Base64.strict_encode64(encrypted_cipher)
-block_length = 31
-puts header
-encoded_cipher.chars.each_slice(block_length) do |block|
-	puts block.join
-end
-puts footer
+#encrypted_cipher = public_key.public_encrypt(ciphertext)
+#header = "------ BEGIN RSA MESSAGE ------"
+#footer = "------ END RSA MESSAGE ------"
+#encoded_cipher = Base64.strict_encode64(encrypted_cipher)
+#block_length = 31
+#puts header
+#encoded_cipher.chars.each_slice(block_length) do |block|
+#	puts block.join
+#end
+#puts footer
 
 # decode using base64
-decoded_cipher = Base64.strict_decode64(encoded_cipher)
+#decoded_cipher = Base64.strict_decode64(encoded_cipher)
 
 # Decryption
-decrypted_cipher = rsa_key.private_decrypt(encrypted_cipher)
-puts "Decrypted Ciphertext: #{decrypted_cipher.unpack('H*').first}"
-decryptext = decrypt(decrypted_cipher, key, iv)
-puts "Decryptext: #{decryptext}"
+#decrypted_cipher = rsa_key.private_decrypt(encrypted_cipher)
+#puts "Decrypted Ciphertext: #{decrypted_cipher.unpack('H*').first}"
+#decryptext = decrypt(decrypted_cipher, key, iv)
+#puts "Decryptext: #{decryptext}"
 
 
