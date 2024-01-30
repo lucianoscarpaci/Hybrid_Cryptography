106688b9ed74ce02ab34784c616798c35fee5ed2
Luciano Scarpaci <16402942+lucianoscarpaci@users.noreply.github.com>
2024-01-30 13:26:17 -0500

    Finished basic encryption and decryption program

--- crypto.rb
+++ crypto.rb
@@ -14,16 +14,7 @@ def encrypt(plaintext, aes_key, aes_iv, rsa_key)
 	rsa_public_key = rsa_key.public_key
 	encrypted_key = rsa_public_key.public_encrypt(aes_key)
 	encoded_data = Base64.strict_encode64(encrypted_data)
-	#block_length = 31
-	#header = "------ BEGIN RSA MESSAGE ------"
-	#block_encoded_data = encoded_data.scan(/.{1,#{block_length}}/).join("\n")
-	#footer = "------ END RSA MESSAGE ------"
-	#message_block = "#{header}\n\n#{block_encoded_data}\n\n#{footer}"
 	encoded_key = Base64.strict_encode64(encrypted_key)
-	#pub_header = "------ BEGIN RSA PUBLIC KEY ------"
-	#block_encoded_key = encoded_key.scan(/.{1,#{block_length}}/).join("\n")
-	#pub_footer = "------ END RSA PUBLIC KEY ------"
-	#key_block = "#{pub_header}\n\n#{block_encoded_key}\n\n#{pub_footer}"
 	return encoded_data, encoded_key
 end
 
@@ -33,7 +24,6 @@ def decrypt(encoded_data, encoded_key, aes_iv, rsa_key)
 	aes_cipher.decrypt
 	aes_cipher.key = aes_key
 	aes_cipher.iv = aes_iv
-	# decode first and then decrypt
 	decrypted_data = aes_cipher.update(Base64.strict_decode64(encoded_data)) + aes_cipher.final
 	return decrypted_data
 end
@@ -41,13 +31,16 @@ end
 print 'Enter the message to be encrypted: '
 plaintext = gets.chomp
 
+puts "Encrypting and Decrypting....."
+puts "Messages received....."
+
 # Encryption
 encoded_data, encoded_key = encrypt(plaintext, aes_key, aes_iv, rsa_key)
 block_length = 31
 header = "------ BEGIN RSA MESSAGE ------"
 block_encoded_data = encoded_data.scan(/.{1,#{block_length}}/).join("\n")
 footer = "------ END RSA MESSAGE ------"
-message_block = "#{header}\n\n#{encoded_data}\n\n#{footer}"
+message_block = "#{header}\n\n#{block_encoded_data}\n\n#{footer}"
 puts "#{message_block}"
 pub_header = "------ BEGIN RSA PUBLIC KEY ------"
 block_encoded_key = encoded_key.scan(/.{1,#{block_length}}/).join("\n")
@@ -57,6 +50,10 @@ puts "#{key_block}"
 
 # Decryption
 decrypted_data = decrypt(encoded_data, encoded_key, aes_iv, rsa_key)
-puts "#{decrypted_data}"
+dec_header = "------ BEGIN DECRYPTED MESSAGE ------"
+block_decrypted_data = decrypted_data.scan(/.{1,#{block_length}}/).join("\n")
+dec_footer = "------ END DECRYPTED MESSAGE ------"
+decrypt_block = "#{dec_header}\n\n#{block_decrypted_data}\n\n#{dec_footer}"
+puts "#{decrypt_block}"
 
 
